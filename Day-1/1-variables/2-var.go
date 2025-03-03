ff05c85bddf17deacaf83df6b0d18d73160cd729
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2025-03-03 20:18:56 +0530

    .

---
+++ Day-1/1-variables/2-var.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// every types have a default value
+	var a int // int default is 0
+	var b string = "ajay"
+	var c = "rahul"
+
+	// go compiler would infer the type automatically from the right side value
+	d := 100 // shorthand
+	{
+		// avoid this , adds confusion
+		//d = "abc"
+		d := "hello" // shadowing
+		d = "abc"
+		fmt.Println(d)
+	}
+	fmt.Println(a, b, c, d)
+}
---
+++ Day-1/1-variables/3-var.go
@@ -0,0 +1,20 @@
+package main
+
+func main() {
+	// if we don't provide any values to the variable,
+	//they get initialized with there default values
+
+	// putting related things together
+	var (
+		name  = "ajay"
+		age   = 30
+		marks = 80.5
+	)
+	_, _, _ = name, age, marks
+
+	// peek into it for design pattern
+	//os.OpenFile()
+	//os.O_APPEND
+	//time.Second
+	//http.StatusOK
+}
