76e9baf0459f11e5493a9300c98dba4a662ae803
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2025-03-05 00:52:06 +0530

    .

--- Day-2/7-interfaces/2-interface-log.go
+++ Day-2/7-interfaces/2-interface-log.go
@@ -1,17 +1,29 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+)
 
 type user struct {
 	name  string
 	email string
 }
 
+func (u user) Write(p []byte) (n int, err error) {
+	fmt.Printf("sending notification to %s %s\n", u.name, u.email)
+	return len(p), nil
+}
+
 func main() {
-	var u user
+	u := user{
+		name:  "Ray",
+		email: "ray@gmail.com",
+	}
 	//log.LstdFlags
 
 	// end goal is this program compiles
 	// user struct variable is passed to log.New
-	log.New(u)
+	l := log.New(u, "log: ", log.LstdFlags)
+	l.Println("Hello, log")
 }
