8054a9787f42982d4ffb41ddfd0c0e6de49a8f50
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2025-03-05 00:26:52 +0530

    .

--- Day-2/7-interfaces/interface.go
+++ Day-2/7-interfaces/interface.go
@@ -12,6 +12,8 @@ import (
 // Few exceptions- If you want to mock for testing,
 //or there are multiple types that need to implement interface immediately
 
+// Bigger the interface weaker the abstraction // Rob Pike
+
 // interface is an abstract type
 
 type Reader interface {
@@ -25,12 +27,16 @@ type Reader interface {
 }
 
 type File struct {
-	name string
+	Name string
 }
 
 func (f File) Read(b []byte) (int, error) {
-	fmt.Println("reading files and processing them", f.name)
+	fmt.Println("reading files and processing them", f.Name)
 	return 0, nil
+}
+
+func (f File) PrintFileName() {
+	fmt.Println(f.Name)
 }
 
 type IO struct {
@@ -49,13 +55,21 @@ func DoReading(r Reader) {
 	// if file is passed Read would be called from the file struct,
 	//or if Io is passed Read would be called from IO
 	n, err := r.Read(nil)
+
+	//f := r.(File) // type assertion // checking if file object is present in the interface or not
+	f, ok := r.(File) // ok would be true if the file object is part of the interface
+	if ok {
+		f.Name = "file2"
+		f.PrintFileName()
+	}
+
 	_, _ = n, err
 	fmt.Printf("%T\n", r)
 }
 
 func main() {
 
-	f := File{name: "file1"}
+	f := File{Name: "file1"}
 	i := IO{name: "io1"}
 	DoReading(f)
 	DoReading(i)
