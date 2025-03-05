7a6c455763afcb5d6f50b5b7e943d08e9e1a0f84
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2025-03-06 02:06:37 +0530

    .

--- Day-3/9-goroutines/15-mutex.go
+++ Day-3/9-goroutines/15-mutex.go
@@ -3,17 +3,22 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
 var x int = 1
+
+// maps are not safe for concurrent access, use mutex for safe operations
+var user = map[int]string{
+	1: "John",
+}
 
 func main() {
 	wg := new(sync.WaitGroup)
 	m := new(sync.Mutex)
 	for i := 0; i < 5; i++ {
-		wg.Add(1)
+		wg.Add(2)
 		go UpdateX(i, wg, m)
+		go UpdateVariable(i, wg)
 	}
 
 	wg.Wait()
@@ -38,7 +43,14 @@ func UpdateX(val int, wg *sync.WaitGroup, m *sync.Mutex) {
 	defer m.Unlock()
 	//the mutex is not locking a specific variable its locking a block of code
 	x = val
-	time.Sleep(1 * time.Second)
+	//time.Sleep(1 * time.Second)
 	fmt.Println(x)
 	// locking till the end of the function
+}
+
+func UpdateVariable(val int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	var abc int
+	abc = val
+	fmt.Println(abc)
 }
