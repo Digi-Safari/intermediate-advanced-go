7b1b9359652a6db657fffa7b32892e24b221817c
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2025-03-06 00:08:37 +0530

    .

--- Day-3/9-goroutines/12-ranges.go
+++ Day-3/9-goroutines/12-ranges.go
@@ -8,14 +8,19 @@ import (
 func main() {
 	ch := make(chan int)
 	wg := &sync.WaitGroup{}
+	wgWorker := &sync.WaitGroup{}
 	wg.Add(2)
 	go func() {
 		defer wg.Done()
 		for i := 1; i <= 5; i++ {
+			wgWorker.Add(1)
 			go func(i int) {
+				defer wgWorker.Done()
 				ch <- i
 			}(i)
 		}
+		wgWorker.Wait()
+		close(ch)
 	}()
 
 	go func() {
