17c764e1ccdb13556363de26c1f8fccd7218c672
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2025-03-06 00:04:04 +0530

    .

--- Day-3/9-goroutines/12-ranges.go
+++ Day-3/9-goroutines/12-ranges.go
@@ -1,10 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
 	ch := make(chan int)
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
 	go func() {
+		defer wg.Done()
 		for i := 1; i <= 5; i++ {
 			go func(i int) {
 				ch <- i
@@ -13,8 +19,11 @@ func main() {
 	}()
 
 	go func() {
+		defer wg.Done()
 		for v := range ch {
 			fmt.Println("received:", v)
 		}
 	}()
+
+	wg.Wait()
 }
