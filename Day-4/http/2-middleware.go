0de28b66af90aa4d5ff9ee924dafa6369ce45803
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2025-03-06 23:59:52 +0530

    .

--- Day-4/http/2-middleware.go
+++ Day-4/http/2-middleware.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 // middleware that exec some pre-processing or the post-processing logic
 // req -> mid1->mid-2-> handler->services
@@ -9,12 +12,30 @@ import "net/http"
 //
 // Middleware Examples: logging, Panic Recovery, Auth, Authorize, GenerateReqID, Fetching Headers
 func main() {
-	http.HandleFunc("/home", Mid(home))
+	http.HandleFunc("/home", Mid(Mid2(home)))
+	panic(http.ListenAndServe(":8080", nil))
+
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Home Page")
 	w.Write([]byte("Home Page"))
 }
-func Mid() {
+func Mid(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Mid layer started")
+		fmt.Println("pre processing logic")
+		next(w, r)
+		fmt.Println("post processing logic")
+		fmt.Println("Mid layer ended")
+	}
 
+}
+
+func Mid2(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Mid2 layer started")
+		next(w, r)
+		fmt.Println("Mid2 layer ended")
+	}
 }
