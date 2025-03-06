86ca29dc86b130eb389a7250cea8543b5cfef14c
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2025-03-07 00:54:36 +0530

    .

--- Day-4/http/3-middleware.go
+++ Day-4/http/3-middleware.go
@@ -19,18 +19,34 @@ func main() {
 
 func RequestIdMid(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
+		ctx := r.Context() // fetching the ctx object from the request
 
 		id := uuid.NewString()
 		// adding a new key value pair in the context
 		ctx = context.WithValue(ctx, RequestIdKey, id)
 		fmt.Println(id)
 		//r.WithContext will update the existing context with the updated one
-		next(w, r.WithContext(ctx))
+		next(w, r.WithContext(ctx)) // calling next thing in the chain
 	}
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
+
+	ctx := r.Context()
+	// fetching the ctx value using the key
+	reqId, ok := ctx.Value(RequestIdKey).(string)
+	if !ok {
+		reqId = "unknown"
+	}
+	fmt.Println("hello handler", reqId)
+	// in case if you
+	AddToDb(ctx) // if other functions need context after handler function,
+	// pass ctx as an argument
 	fmt.Fprintln(w, "Hello")
-	fmt.Println(reqId)
+
+}
+
+func AddToDb(ctx context.Context) {
+	//sql.DB{}.ExecContext(ctx)
+
 }
